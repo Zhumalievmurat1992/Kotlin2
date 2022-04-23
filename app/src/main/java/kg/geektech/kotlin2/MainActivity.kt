bfb0e85734f9690632aef28f60793aed629e4b3d
User <Zhumalievmurat1992@gmail.com>
2022-04-23 20:27:00 +0600

    refactorCod

--- app/src/main/java/kg/geektech/kotlin2/MainActivity.kt
+++ app/src/main/java/kg/geektech/kotlin2/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val imageList = arrayListOf<String>()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
